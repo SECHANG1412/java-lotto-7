f22e86d3bee2780fbb724c50f495aba49c02fe5f
SECHANG1412 <binaryblitz1412@gmail.com>
2024-11-04 19:16:55 +0900

    feat: Lotto 클래스에 중복 검사 기능 추가 및 번호 정렬 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -8,13 +9,24 @@ public class Lotto {
     public Lotto(List<Integer> numbers) {
         validate(numbers);
         this.numbers = numbers;
+        Collections.sort(this.numbers);
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (hasDuplicates(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean hasDuplicates(List<Integer> numbers) {
+        return numbers.size() != numbers.stream().distinct().count();
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
+
